243460cf7a731442ccc57598dc31cdcc739c800c
lfarina <lfarina@student.42angouleme.fr>
2026-01-14 16:39:04 +0100

    :hammer: add a board to the player

--- server/src/game/Player.js
+++ server/src/game/Player.js
@@ -1,8 +1,13 @@
+const {Piece} = require("./Piece");
+
 class Player {
     constructor(username, socketId) {
         this.username = username;
         this.socketId = socketId;
         this.isMaster = false;
+        this.board = Array.from({ length: 10 * 20 }, (_, index) => 0);;
+        this.currentPiece = null;
+        this.pieceId = 0;
     }
 
     switchMasterStatus(status) {
