942e8fd639851ee33b2b8325204b0643df49a79d
lfarina <lfarina@student.42angouleme.fr>
2026-01-14 16:10:17 +0100

    :hammer: correct code

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,17 @@
 // client/src/index.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import gameReducer from './store/reducers';
 import socketMiddleware from './store/socketMiddleware';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from 'react-router-dom';
 
 
 const AppWithNavigate = () => {
     const navigate = useNavigate();
+    const location = useLocation();
 
     const socketMiddlewareWithNavigate = socketMiddleware(navigate);
 
@@ -21,6 +22,13 @@ const AppWithNavigate = () => {
         middleware: (getDefaultMiddleware) =>
             getDefaultMiddleware().concat(socketMiddlewareWithNavigate),
     });
+
+    useEffect(() => {
+        const pathParts = location.pathname.split('/').filter(Boolean);
+        if (pathParts.length !== 2) {
+            navigate('/');
+        }
+    }, [location.pathname, navigate]);
 
     return (
         <Provider store={store}>
